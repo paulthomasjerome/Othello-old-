0668978b80cff239ed2f2735a7943364ae6b865d
Paul Jerome <mr.paulthomasjerome@gmail.com>
2020-03-27 15:58:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,36 @@ let player = 'black';
 //instantiate counter for filled spaces
 let discCounter = 4;
 
-// draw the gameboard
-const drawBoard = () => {
-
+/** 
+ * Renders the current board state to the users viewport
+*/
+const drawBoard = board => {
+  //for each row of the board
+    //add this row to the board
+    //for each space in this row
+      //add a cell to this row
+      //add appropriate classes 
+      //index the cell with data atributes
 }
 
 //place a disc
 const discPlacement = player => {
   //if the user clicks on a square
-  $('.placement').click(function(){
-    //if this is an open space
-    if($(this).css('background-color') === 'rgb(0, 128, 0)') {
+  $('.square').click(function(){
+    //if this is an open space 
+    //DELCOM maybe instead of checking the color we should check the logical state?
+    if($(this).children().css('background-color') === 'rgb(0, 128, 0)') {
       //if the current player is black
       if(player === 'black') {
         //draw black disk in this space
-        $(this).toggleClass('black');
+        $(this).children().toggleClass('black');
         //increase disc counter by 1
         discCounter++;
         //change player
         player = 'white';
       } else {
         //draw white disk in this space
-        $(this).toggleClass('white');
+        $(this).children().toggleClass('white');
         //change player
         player = 'black';
         //increase disc counter by 1
@@ -35,10 +43,42 @@ const discPlacement = player => {
   });
 }
 
+/**
+ * Initializes the initial state of the game and renders the board based on that state
+ */
+const startGame = () => {
+  //set the initial state of the logical board
+  const board = [
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ','w','b',' ',' ',' '],
+    [' ',' ',' ','b','w',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],    
+  ];
+
+  drawBoard(board);
+
+  //for each row of the board
+    //for each space in this row
+      //if a user clicks on this space
+        //if this space is empty
+          //if this is player black
+
+          //if this is player white
+        //else
+          //alert the user that this space is occupied
+
+}
+
 //flip over pieces depending on the board state
 const updateState = () => {
 
 }
 
-
+startGame();
+//DELCOM we may have to encapsulate this in its own function to be part of the board drawing method
 discPlacement(player);
+updateState();
